7702940ea5ed5b02a46c097a9db145c09177b72b
dudatiemiak <rm554756@fiap.com.br>
2024-08-08 08:20:32 -0300

    Criando elementos e preenchendo a lista

--- js/script.js
+++ js/script.js
@@ -4,7 +4,11 @@ let alunos= ["Leticia", "Mariana", "Pedro", "Felipe", "Vitor", "Enrico", "Israel
 //Recuperando a lista ul que está no HTML de id 'lista'
 let lista = document.getElementById('lista');
 
-//Criar um elemento li para ser adicionado a lista
-let elementoLi = document.createElement("li");
-//Adicionando texto ao elemento li
-elementoLi.textContent = "Novo Aluno";
+//Preenchendo a lista com forEach
+alunos.forEach((aluno)=>{
+    //Criar um elemento li para ser adicionando a lista
+    let elementoLi = document.createElement("li");
+     //Adicionando texto ao elemento li
+    elementoLi.textContent = aluno;
+    lista.appendChild(elementoLi);
+});
